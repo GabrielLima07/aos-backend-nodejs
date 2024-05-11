b51d8b76144bf33dd571a93386c0aeb4b195cd4d
Gabriel Lima <102686294+GabrielLima07@users.noreply.github.com>
2024-05-11 12:47:21 -0300

    additional project setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import express from 'express';
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
